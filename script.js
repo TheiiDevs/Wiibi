5c3000e3bc45b1d732f3ebe10ff5d69cbd1a243b
David-Unah <davidunah022@gmail.com>
2023-08-26 08:18:02 +0100

    updated company page

--- script.js
+++ script.js
@@ -47,66 +47,6 @@ let answers = document.querySelectorAll(".faqboxes");
    })
 
 
-   /* this is for the automatic slide show */
-
-  /*  let slideIndex = 0;
-
-   function showSlides() {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 5000); // Change image every 5 seconds
-  }
-
-showSlides(); */
-
-
-
- /* this is for the header manual slides */
-
-
-/*  let slideNum = 1;
- headslides(slideNum);
- 
- function plusSlides(n) {
-     headslides(slideNum += n);
- }
- 
- function currentSlide(n) {
-     headslides(slideNum = n);
- }
- 
- function headslides(n) {
-   let i;
-   let slidesbox = document.getElementsByClassName("mySlides");
-   let dots = document.getElementsByClassName("dots");
-   if (n > slidesbox.length) {slideNum = 1}    
-   if (n < 1) {slideNum = slidesbox.length}
-   for (i = 0; i < slidesbox.length; i++) {
-     slidesbox[i].style.display = "none";  
-   }
-   for (i = 0; i < dots.length; i++) {
-     dots[i].className = dots[i].className.replace(" active", "");
-   }
-   slidesbox[slideNum-1].style.display = "block";  
-   dots[slideNum-1].className += " active";
-
-  }
- */
-
-
-
-
 
   let slideIndex = 0;
   showSlides();
